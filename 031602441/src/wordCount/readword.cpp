12f6db5d67af80d25e80c65fc320dc0ea78a52f0
anmui <893724023@qq.com>
2018-09-16 20:30:07 +0800

    031602441

--- 031602441/src/wordCount/readword.cpp
+++ 031602441/src/wordCount/readword.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 
 /**********************读取文件中单词*********************/
-int readword(struct word*&head, char *filename, int word_num)
+int readword(struct word*&head,char *filename, int word_num)
 {
 	int flag = 0;
+	int flag1 = 0;
+	int flag2 = 0;
+	int i = 0;
 	FILE *fp;
 	if ((fp = _fsopen(filename, "r", _SH_DENYNO)) == NULL)
 	{
@@ -16,10 +19,13 @@ int readword(struct word*&head, char *filename, int word_num)
 		exit(0);
 	}
 	char ch, b_chars[30];
-	struct word *p;
+	struct word *p=new word;
+	p->next = NULL;
+	struct word *chars[30];
+	int z = 1;
 	while (!feof(fp))
 	{
-		int i = 0;
+		i = 0;
 		ch = fgetc(fp);
 		while ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z') || (ch >= '0'&&ch <= '9'))
 		{
@@ -56,26 +62,75 @@ int readword(struct word*&head, char *filename, int word_num)
 			}
 		}
 		p = head->next;
-		while (p&&flag == 1)
+		struct word *q = new word;
+		q->next = NULL;
+		if (flag == 1 && b_chars[0] != '\0')
 		{
-			if (!_stricmp(b_chars, p->name))
+			for (int j = 1;j < z;j++)
 			{
-				p->num++;break;
+				if (chars[j]->name[0]== b_chars[0])
+				{
+					flag1 = j;
+					break;
+				}
 			}
-			p = p->next;
+			if (flag1!=0)
+			{
+				strcpy_s(q->name, b_chars);
+				q->num = 1;
+				q->next = chars[flag1]->next;
+				chars[flag1]->next = q;
+			}
+			else
+			{
+				strcpy_s(q->name, b_chars);
+				q->num = 1;
+				q->next = head->next;
+				head->next = q;
+				chars[z] = new word;
+				chars[z] = q;
+				z++;
+
+			}
 		}
-		if (!p&&flag == 1 && b_chars[0] != '\0')
-		{
-			p = new word;
-			strcpy_s(p->name, b_chars);
-			p->num = 1;
-			p->next = head->next;
-			head->next = p;
-		}
+		flag1 = 0;
 		flag = 0;
+		
 	}
+	printf("单词录入完毕！\n");
+	struct word* p1 = new word;
+	p = head->next;
+	while (p)
+	{
+		p1 = p->next;
+		if (p->num != 0)
+		{
+			while (p1 != NULL)
+			{
+				if (p1->name[0] == p->name[0])
+				{
+					if (p1->num != 0)
+					{
+						if (!_stricmp(p->name, p1->name))
+						{
+							p->num++;
+							p1->num = 0;
+						}
+					}
+					p1 = p1->next;
+				}
+				else
+					break;
+			}
+		}
+		p = p->next;
+	}
+	printf("统计完成");
 	std::ofstream openfile("result.txt", std::ios::app);
 	openfile << "words:" << word_num << endl;
 	openfile.close();
 	return 0;
+	
 }
+
+
--- 031602441/src/wordCount/sort.cpp
+++ 031602441/src/wordCount/sort.cpp
@@ -8,51 +8,48 @@ using namespace std;
 int sort(struct word *&head)
 {
 	struct word *q;
-	struct word a[10];
+	struct word *a[10];
+	struct word *q1 = new word;
 	int i;
 	char s[30] = { 0 };
 	for (i = 0;i < 10;i++)
 	{
-		a[i].num = 0;
-		strcpy_s(a[i].name, s);
+		a[i] = new word;
+		a[i]->num = 0;
+		strcpy_s(a[i]->name, s);
 	}
 	for (i = 0;i < 10;i++)
 	{
-		q = head;
+		q = head->next;
 		while (q != NULL)
 		{
-			if (q->num == a[i].num)
+			if (q->num == a[i]->num)
 			{
-				if (_stricmp(q->name, a[i].name) < 0)
+				if (_stricmp(q->name, a[i]->name) < 0)
 				{
-					strcpy_s(a[i].name, q->name);
+					strcpy_s(a[i]->name, q->name);
+					a[i]->num = q->num;
+					q1 = q;
 				}
 			}
-			else if (q->num > a[i].num)
+			else if (q->num > a[i]->num)
 			{
-				strcpy_s(a[i].name, q->name);
-				a[i].num = q->num;
-
+				strcpy_s(a[i]->name, q->name);
+				a[i]->num = q->num;
+				q1 = q;
 			}
 			q = q->next;
 		}
-		q = head;
-		while (q != NULL)
+		q1->num = 0;
+	}
+	for (i = 0;i < 10;i++)
+	{
+		if (a[i]->num != 0)
 		{
-			if (_stricmp(a[i].name, q->name) == 0)
-			{
-				q->num = 0;
-				if (a[i].num != 0)
-				{
-					std::ofstream openfile("result.txt", std::ios::app);
-					openfile << "<" << a[i].name << ">:" << a[i].num << endl;
-					openfile.close();
-				}
-				break;
-			}
-			else
-				q = q->next;
+			std::ofstream openfile("result.txt", std::ios::app);
+			openfile << "<" << a[i]->name << ">:" << a[i]->num << endl;
+			openfile.close();
 		}
-		
-	}return 0;
+	}
+	return 0;
 }
